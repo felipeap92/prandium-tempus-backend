ac4825af9b466f9151841a6e9058f936d168c67a
Felipe de Assis Pinto <felipeap92@yahoo.com.br>
2019-05-04 01:02:57 -0300

    Add fake.js to generate fake users to seed prisma database

--- prisma/seed/user.seed.ts
+++ prisma/seed/user.seed.ts
@@ -1,29 +1,23 @@
+import faker = require('faker');
+
 import { prisma, UserCreateInput } from './../../src/generated/prisma-client';
 import User from '../../src/user/user';
 
 /**
  * Manages User seed.
- *
- * These fake users where created using: https://randomuser.me/api/
  */
 export class UserSeed {
   /**
    * Populates prisma database with fake users.
    */
   public static CreateUsers(): Promise<User[]> {
-    return Promise.all([
-      prisma.createUser(new User('Christian Stevens', 'christian.stevens@example.com',
-        'https://randomuser.me/api/portraits/men/40.jpg') as UserCreateInput),
-      prisma.createUser(new User('Hadrien Blanchard', 'hadrien.blanchard@example.com',
-        'https://randomuser.me/api/portraits/men/39.jpg') as UserCreateInput),
-      prisma.createUser(new User('Robbert Bullens', 'robbert.bullens@example.com',
-        'https://randomuser.me/api/portraits/men/32.jpg') as UserCreateInput),
-      prisma.createUser(new User('Thea Jorgensen', 'thea.jorgensen@example.com',
-        'https://randomuser.me/api/portraits/women/12.jpg') as UserCreateInput),
-      prisma.createUser(new User('Elif Akyüz', 'elif.akyüz@example.com',
-        'https://randomuser.me/api/portraits/women/10.jpg') as UserCreateInput),
-      prisma.createUser(new User('Noelle Day', 'noelle.day@example.com',
-        'https://randomuser.me/api/portraits/women/30.jpg') as UserCreateInput),
-    ]);
+    const promises = new Array<Promise<User>>();
+    for (let count = 0; count < 25; count++) {
+      const userFirstName = faker.name.firstName();
+      const userLastName = faker.name.lastName();
+      const user = new User(`${userFirstName} ${userLastName}`, faker.internet.email(userFirstName, userLastName), faker.internet.avatar());
+      promises.push(prisma.createUser(user as UserCreateInput));
+    }
+    return Promise.all([...promises]);
   }
 }
