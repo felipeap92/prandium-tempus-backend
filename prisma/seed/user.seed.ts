af7e2ac9a362f5277e9b66f9cb14ae4f53fec4d8
oliveiraeloi <eloi.antonio.oliveira@gmail.com>
2019-05-17 16:27:53 -0300

    Simplified syntax

--- prisma/seed/user.seed.ts
+++ prisma/seed/user.seed.ts
@@ -11,13 +11,13 @@ export class UserSeed {
    * Populates prisma database with fake users.
    */
   public static CreateUsers(): Promise<User[]> {
-    const promises = new Array<Promise<User>>();
+    const promises: Promise<User>[] = [];
     for (let count = 0; count < 25; count++) {
       const userFirstName = faker.name.firstName();
       const userLastName = faker.name.lastName();
       const user = new User(`${userFirstName} ${userLastName}`, faker.internet.email(userFirstName, userLastName), faker.internet.avatar());
       promises.push(prisma.createUser(user as UserCreateInput));
     }
-    return Promise.all([...promises]);
+    return Promise.all(promises);
   }
 }
